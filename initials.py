44b2340bfd565e494a94700ede80d357046c0dcf
GODFRED397 <2425402076@live.gctu.edu.gh>
2026-05-30 23:36:20 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: MENSAH GODFRED
+# STUDENT ID:  2425402076
+# Email: 2425402076@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, MENSAH GODFRED, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in small and large stylized block letters using only print and strings
 
+  
+    print()
+    print("...GMA")
+    print()
+    print("....GGGGGGGGG.........MM......MM.............AAA............")
+    print("...GG.................MMM....MMM............A...A...........")
+    print("...G..................M.MM..MM.M...........A.....A..........")
+    print("...G..................M..MMMM..M..........A.......A.........")
+    print("...G....GGGGG.........M...MM...M.........AAAAAAAAAA.........")
+    print("...G........G.........M........M.........A.........A........")
+    print("...G........G.........M........M.........A.........A........")
+    print("...G........G.........M........M.........A.........A........")
+    print("...GG.......G..****...M........M..****...A.........A.****...")
+    print("....GGGGGGGGG..****...M........M..****...A.........A.****...")
+    print()
 
 
 main()
